a638b863fe2a47d276cd5f287c1a638800bd907e
mouni <mounika.g1162@gmail.com>
2023-07-05 23:01:13 +0530

    change

--- frontend/src/compo/login/global.js
+++ frontend/src/compo/login/global.js
@@ -1,2 +1,3 @@
-export const API="https://stack-webcode-8auy.vercel.app"
+// export const API="https://stack-webcode-8auy.vercel.app"
+export const API="https://stack-webcode-tv3m.vercel.app"
 // export const API="http://localhost:4000"
